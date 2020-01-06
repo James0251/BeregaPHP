13982840c10a8c56947f687299eb043d1807b8a5
James0251 <den4643025@mail.ru>
2020-01-06 19:20:49 +0300

    Новый токен

--- bereg.php
+++ bereg.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/vendor/autoload.php';
 
 use GuzzleHttp\Client as Client;
 
-$token = 'c38aa09bc38aa09bc38aa09b74c3e44553cc38ac38aa09b9d84b30e048f8ec39a7eaba6';
+$token = '456058734560587345605873f8450ebdbb44560456058731b7305a82c940e0aa7b825bf';
 $version = 5.103;
 $domain = 'beregaonline_tutaev';
 
@@ -21,6 +21,6 @@ $url = 'https://api.vk.com/method/wall.get?'. http_build_query($params);
 $data = file_get_contents($url);
 //var_dump(json_decode($json));
 $obj = json_decode($data,true);
-$title_one = $obj['response']['items'][0]['copy_history'][0]['text'];
-var_dump($title_one);
+$title_one = $obj['response'];
+var_dump($obj);
 
